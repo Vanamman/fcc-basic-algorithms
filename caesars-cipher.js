bd9d59dcd5fd271da143cfa85fe131caa9db55d1
Timothy Ott <vanamman06@gmail.com>
2018-01-28 20:39:28 -0600

    Made code more readable

--- caesars-cipher.js
+++ caesars-cipher.js
@@ -2,12 +2,13 @@ function rot13(str) { // LBH QVQ VG!
     var x;
     var y = "";
     for (var i = 0; i < str.length; i++){
-        if ((str.charCodeAt(i)) > 77) {
-            x = (str.charCodeAt(i) - 13);
-        } else if ((str.charCodeAt(i)) > 64 ){
-            x = (str.charCodeAt(i) + 13);
-        } else if (str.charCodeAt(i) < 64 || str.charCodeAt(i) > 90){
-            x = str.charCodeAt(i);
+        var char = str.charCodeAt(i);
+        if ((char) > 77) {
+            x = (char - 13);
+        } else if ((char) > 64 ){
+            x = (char + 13);
+        } else if (char < 64 || char > 90){
+            x = char;
         }
         y += String.fromCharCode(x);
     }
